9d9698e2b15b20a996004bd01271e8a0b4d682be
Meghansh Govil <govilmeghansh@gmail.com>
2024-10-31 20:49:12 +0530

    October 12th

---
+++ LeetCode_Daily_2406.cpp
@@ -0,0 +1,15 @@
+/* Question -> Divide Intervals Into Minimum Number of Groups */
+class Solution {
+public:
+    int minGroups(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (const auto& interval : intervals) {
+            if (!pq.empty() && pq.top() < interval[0]) {
+                pq.pop();
+            }
+            pq.push(interval[1]);
+        }
+        return pq.size();
+    }
+};
